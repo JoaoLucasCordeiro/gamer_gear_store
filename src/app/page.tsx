5b4f6af0930b5128a415cec5dcc860707f56a95e
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-10-31 08:48:34 -0300

    chore: fisrt migration of database and tailwind configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-  <main>
-    <h1>Gamer Gear Store</h1>
-  </main>
+    <main>
+      <h1>Gamer Gear Store</h1>
+    </main>
   )
 }
