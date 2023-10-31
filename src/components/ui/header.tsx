904ce75bde139485ae34d6500aa9a1e594a602d8
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-10-31 11:35:07 -0300

    feat: add auth on the Header component

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -1,11 +1,25 @@
-import { MenuIcon, ShoppingCartIcon, LogInIcon, PercentIcon, ListOrderedIcon, HomeIcon } from "lucide-react";
+"use client"
+
+import { MenuIcon, ShoppingCartIcon, LogInIcon, LogOutIcon, PercentIcon, ListOrderedIcon, HomeIcon } from "lucide-react";
 import { Button } from "./button";
 import { Card } from "./card";
 import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "./sheet";
-
-
+import { signIn, signOut, useSession } from "next-auth/react"
+import { Avatar, AvatarFallback, AvatarImage } from "@radix-ui/react-avatar";
+import { Separator } from "@radix-ui/react-separator";
 
 const Header = () => {
+
+    const handleLoginClick = async () => {
+        await signIn()
+    }
+
+    const handleLogoutClick = async () => {
+        await signOut()
+    }
+
+    const { status, data } = useSession()
+
     return (
         <Card className="flex justify-between p-[1.875rem] items-center">
             <Sheet>
@@ -16,15 +30,43 @@ const Header = () => {
                 </SheetTrigger>
 
                 <SheetContent side="left">
+
                     <SheetHeader className="text-left text-lg font-semibold">
                         Menu
                     </SheetHeader>
 
-                    <div className="mt-2 flex flex-col gap-3">
-                        <Button variant="outline" className="w-full justify-start gap-2">
-                            <LogInIcon />
-                            Fazer Login
-                        </Button>
+                    {status === "authenticated" && data?.user && (
+                        <div className="flex flex-col">
+                            <div className="flex items-center gap-2 py-4">
+                                <Avatar>
+                                    <AvatarFallback>
+                                        {data.user.name?.[0].toUpperCase()}
+                                    </AvatarFallback>
+
+                                    {data.user.image && <AvatarImage src={data.user.image} className="w-[50px]" />}
+                                </Avatar>
+
+                                <p className="font-medium text-primary">{data.user.name}</p>
+                            </div>
+                            <Separator />
+                        </div>
+                    )}
+
+                    <div className="mt-4 flex flex-col gap-3">
+
+                        {status === "unauthenticated" && (
+                            <Button variant="outline" className="w-full justify-start gap-2" onClick={handleLoginClick}>
+                                <LogInIcon />
+                                Fazer Login
+                            </Button>
+                        )}
+
+                        {status === "authenticated" && (
+                            <Button variant="outline" className="w-full justify-start gap-2" onClick={handleLogoutClick}>
+                                <LogOutIcon />
+                                Fazer Logout
+                            </Button>
+                        )}
 
                         <Button variant="outline" className="w-full justify-start gap-2">
                             <HomeIcon />
