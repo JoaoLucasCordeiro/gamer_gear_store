beeaabc08fa7e578e7cbc075a4c2841817f85766
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-10-31 17:07:33 -0300

    feat: add catalog page

--- src/app/catalog/page.tsx
+++ src/app/catalog/page.tsx
@@ -1,6 +1,26 @@
-const CatalogPage = () => {
+import { Badge } from "@/components/ui/badge";
+import { prismaClient } from "@/lib/prisma";
+import { ShapesIcon } from "lucide-react";
+import CategoryItem from "./components/categoryItem";
+
+
+const CatalogPage = async () => {
+
+    const categories = await prismaClient.category.findMany({})
+
     return (
-        <h1>Catálogo</h1>
+        <div className="p-5 flex flex-col gap-8">
+            <Badge className="w-fit gap-1 text-base uppercase border-2 border-primary px-3 py-[0.375rem]" variant="outline">
+                <ShapesIcon size={16} />
+                Catálogo
+            </Badge>
+
+            <div className="grid grid-cols-2 gap-8">
+                {categories.map((category) => (
+                    <CategoryItem key={category.id} category={category} />
+                ))}
+            </div>
+        </div>
     );
 }
 
