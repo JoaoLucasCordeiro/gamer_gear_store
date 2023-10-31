db565e08855ed1ddfb65bd8f5b1759814c4a670b
Lucas Cordeiro <joaolucascttprofissional@gmail.com>
2023-10-31 15:46:29 -0300

    feat: add ProductItem to the ProductList component

---
+++ src/components/ui/productItem.tsx
@@ -0,0 +1,59 @@
+import { ProductWithTotalPrice } from "@/helpers/product";
+import { ArrowDownIcon } from "lucide-react";
+import Image from "next/image"
+import { Badge } from "./badge";
+
+interface ProductItemProps {
+    product: ProductWithTotalPrice
+}
+
+const ProductItem = ({ product }: ProductItemProps) => {
+    return (
+        <div className="flex flex-col gap-4 max-w-[156px]">
+            <div className="relative bg-accent rounded-lg h-[170px] w-[156px] flex items-center justify-center">
+                <Image
+                    src={product.imageUrls[0]}
+                    height={0}
+                    width={0}
+                    sizes="100vw"
+                    className="h-auto max-h-[70%] w-auto max-w-[80%] object-contain"
+                    alt={product.name}
+                />
+
+
+                {product.discountPercentage > 0 && (
+                    <Badge className="absolute left-2 top-2 text-sm flex items-center px-2 py-[2px]">
+                        <ArrowDownIcon size={12} />
+                        {product.discountPercentage} %
+                    </Badge>
+                )}
+            </div>
+
+            <div className="flex flex-col gap-1">
+                <p className="overflow-hidden text-ellipsis whitespace-nowrap text-sm">{product.name}</p>
+
+                <div className="flex items-center justify-center">
+                    {product.discountPercentage > 0 ? (
+                        <>
+                            <p className="truncate font-semibold lg:text-lg">
+                                R$ {product.totalPrice.toFixed(2)}
+                            </p>
+
+                            <p className="truncate text-xs line-through opacity-75 lg:text-sm">
+                                R$ {Number(product.basePrice).toFixed(2)}
+                            </p>
+                        </>
+                    ) : (
+                        <p className="truncate text-sm font-semibold">
+                            R$ {product.basePrice.toFixed(2)}
+                        </p>
+                    )}
+                </div>
+            </div>
+
+
+        </div>
+    );
+}
+
+export default ProductItem;
